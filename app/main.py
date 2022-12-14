f6058ca8eea05506483a72b2cfddc45f19799ecc
Peter_Brotherhood <peter.r.brotherhood@student.uts.edu.au>
2022-12-14 20:37:40 +1100

    heroku

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 from starlette.responses import JSONResponse
 from joblib import load
 import pandas as pd
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from model import PytorchMultiClass
+from mod import PytorchMultiClass
 
 app = FastAPI()
 
@@ -24,9 +25,9 @@ preprocessor = load('../models/preprocessor.joblib')
 
 #device = torch.device("cpu")
 
-model = PytorchMultiClass(14)
-model.load_state_dict(torch.load("../models/pytorch_beer_classifier.pt"))  #, map_location=device))
-# model.eval()
+mod = PytorchMultiClass(14)
+mod.load_state_dict(torch.load("../models/pytorch_beer_classifier.pt"))  #, map_location=device))
+mod.eval()
 
 @app.get("/")
 def read_root():
@@ -36,7 +37,7 @@ def read_root():
 def healthcheck():
     return 'Beer classification is ready to go!'
 
-def format_features(brewery_name: str,	review_aroma: int, review_appearance: int, review_palate: int, review_taste: int, beer_abv: int):
+def format_features(brewery_name: str,	review_aroma: float, review_appearance: float, review_palate: float, review_taste: float, beer_abv: float):
     return {
         'brewery_name': [brewery_name],
         'review_aroma': [review_aroma],
@@ -47,12 +48,28 @@ def format_features(brewery_name: str,	review_aroma: int, review_appearance: int
     }
 
 @app.get("/beer/type")
-def predict(brewery_name: str,	review_aroma: int, review_appearance: int, review_palate: int, review_taste: int, beer_abv: int):
+def predict(brewery_name: str,	review_aroma: float, review_appearance: float, review_palate: float, review_taste: float, beer_abv: float):
     features = format_features(brewery_name, review_aroma, review_appearance, review_palate, review_taste, beer_abv)
     obs = pd.DataFrame(features)
     data = preprocessor.transform(obs)
-    # data_tensor = torch.Tensor(np.array(data))
-    # pred = model(data_tensor).argmax(1)
-    # return JSONResponse(pred.tolist())
-    return JSONResponse(data.tolist())
+    data_tensor = torch.Tensor(np.array(data))
+    pred = mod(data_tensor).argmax(1)
+    return JSONResponse(pred.tolist())
+    # return JSONResponse(data.tolist())
     # return data
+
+@app.get("/beers/type")
+def predict(brewery_name: str,	review_aroma: float, review_appearance: float, review_palate: float, review_taste: float, beer_abv: float, brewery_name_2: str,	review_aroma_2: float, review_appearance_2: float, review_palate_2: float, review_taste_2: float, beer_abv_2: float):
+    features = format_features(brewery_name, review_aroma, review_appearance, review_palate, review_taste, beer_abv)
+    features2 = format_features(brewery_name_2, review_aroma_2, review_appearance_2, review_palate_2, review_taste_2, beer_abv_2)
+    obs_1 = pd.DataFrame(features)
+    obs_2 = pd.DataFrame(features2)
+    obs = obs_1.append(obs_2)
+    data = preprocessor.transform(obs)
+    data_tensor = torch.Tensor(np.array(data))
+    pred = mod(data_tensor).argmax(1)
+    return JSONResponse(pred.tolist())
+
+@app.get("/model/architecture")
+def architecture():
+    return JSONResponse(str(mod))
